c25ff79f838593a1473dd2e655657455f4cbf2e4
Student <student@donation.local>
2019-11-07 12:50:26 -0600

    First Commit to new project. Made some models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Coffee(models.Model):
+    item = models.TextField()
+    price = models.FloatField()
+
+class Transaction(models.Model):
+    time = models.DateTimeField()
+    coffee = models.ForeignKey(Coffee, on_delete=models.CASCADE)
+    pre_tax = models.FloatField()
+    tax = models.FloatField()
